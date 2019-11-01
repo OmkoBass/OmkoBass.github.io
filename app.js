f4471c53089508858423ff1c80dd6da51ae40ddb
OmkoBass <omkob@hotmail.com>
2019-11-01 21:22:45 +0100

    Website
    Added jquery. Redid the entire page with bootstrap

---
+++ app.js
@@ -0,0 +1,21 @@
+function validate()
+{
+    var msg = document.getElementById("messagebox").value;
+    var success = document.getElementById("alertsuccess");
+    var danger = document.getElementById("alertdanger");
+
+    if(msg !== "")
+    {
+        success.style.opacity = "1";
+        danger.style.opacity = "0";
+        success.style.height = "60px";
+        danger.style.height = "0px";
+    }
+    else
+    {
+        success.style.opacity = "0";
+        danger.style.opacity = "1";
+        danger.style.height = "60px";
+        success.style.height = "0px";
+    }
+}
